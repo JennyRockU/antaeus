f2905a803aa14a6b5b4f586c919b9b9617d6b282
JennyRockU <jennyrukman@gmail.com>
2019-06-03 21:25:35 +0300

    Initial flow of billing service to charge unpaid invoices

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -6,4 +6,52 @@ class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
    // TODO - Add code e.g. here
+    
+        fun chargeInvoices() {
+
+        // TODO handle localization
+        val date = java.time.LocalDate.now()
+        val day = date.getDayOfMonth()
+
+        // bill only on the first day of the month
+        val isBillingPeriod = day == 1;
+
+        if (isBillingPeriod){
+
+            // get all the pending invoices
+            var invoicesToProcess = customerService.fetch();
+            var pending : List<Invoice> = invoicesToProcess
+                .filter{ it.status == InvoiceStatus.PENDING.toString() }
+
+            // TODO try-catch and retry charge
+            for (invoice in invoicesToProcess){
+
+                // TODO pass relevant parameters to 'charge' (customer, currency, amount)
+                var charged = false
+                try {
+
+                    charged = PaymentProvider.charge(invoice);
+                    if (charged) {
+
+                        //TODO handle expetions/retry (customer should not be charged without
+                        // the invoice to be marked as 'paid'
+                        // mark invoice as 'PAID'
+                        invoiceService.update(invoice.id)
+                    }
+
+                } catch (e: CustomerNotFoundException){
+                    // TODO call logger
+                } catch (e: CurrencyMismatchException){
+                    // TODO call logger
+                } catch (e: NetworkException){
+                    // TODO call logger
+                } catch (e: Exception){
+                    // TODO call logger
+                }
+
+            }
+        }
+
+    }
+    
 }
