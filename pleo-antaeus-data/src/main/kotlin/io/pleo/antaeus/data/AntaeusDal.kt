6e8fb4be87f0f2c85d72280bad1a9ba7f979c034
JennyRockU <jennyrukman@gmail.com>
2019-06-04 16:03:58 +0300

    Added invoice update and update of bank account
    The tryUpdateBankAccount is meant to represent a transaction of an External database which is not part of Antaeus - therefore, adding it under Dal, is a workaround for the sake of simplicity of this challenge.

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -12,6 +12,7 @@ import io.pleo.antaeus.models.Customer
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
+import io.pleo.antaeus.models.bank.BankAccount
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
@@ -52,6 +53,17 @@ class AntaeusDal(private val db: Database) {
 
         return fetchInvoice(id!!)
     }
+    
+    fun updateInvoice(id: Int, status: InvoiceStatus){
+        transaction(db) {
+            // Update the invoice status
+            InvoiceTable
+                .select{ InvoiceTable.id.eq(id) }
+                .update {
+                    it[this.status] = status.toString()
+            }
+        }
+    }
 
     fun fetchCustomer(id: Int): Customer? {
         return transaction(db) {
@@ -79,5 +91,24 @@ class AntaeusDal(private val db: Database) {
         }
 
         return fetchCustomer(id!!)
+    }
+    
+    
+    // External, updates "external" model of a customer bank account
+    // when update fails due to low funds, 'false' is returned. While this is not a desired outcome
+    // this is still a behavior that can be expected, therefore no execption is thrown when that happens.
+    fun tryUpdateBankAccount(id: Int, amount: java.math.BigDecimal): Boolean? {
+       return transaction(db) {
+            // Update the account's balance
+           var account = BankAccountTable
+               .select { BankAccountTable.id.eq(id) }
+               .firstOrNull()
+
+           if ( account == null) return null
+           if (account.balance < amount) return false
+
+           account[this.balance] = account.balance - amount
+           return true
+        }
     }
 }
