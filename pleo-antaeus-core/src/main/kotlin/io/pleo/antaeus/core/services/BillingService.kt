4183764cc5fa6278a41ab62eb50b6a1e5674e4a5
JennyRockU <jennyrukman@gmail.com>
2019-06-07 15:47:34 +0300

    Retry on updating billed invoice, optimized pending invoices' fetching.
    Added a retry upon updating a billed invoice, optimized the way pending invoices are fetched, changed local time to utc, removed exception handling to propagate outside.

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,57 +1,55 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
-import io.pleo.antaeus.core.services.InvoiceService
+import io.pleo.antaeus.models.InvoiceStatus
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 class BillingService(private val paymentProvider: PaymentProvider, private val invoiceService: InvoiceService) {
-    
-   // TODO - Add code e.g. here
-    
-   fun chargeInvoices() {
 
-        // TODO handle localization
-        val date = java.time.LocalDate.now()
-        val day = date.getDayOfMonth()
+    /*
+    Throws:
+    chargeInvoices can throw the following exceptions, which will be propagated to the caller (rest)
+          `CustomerNotFoundException`: when no customer has the given id.
+          `CurrencyMismatchException`: when the currency does not match the customer account.
+          `NetworkException`: when a network error happens.
+     */
+    fun chargeInvoices() {
+
+        val date = LocalDate.now(ZoneOffset.UTC)
+        val day = date.dayOfMonth
 
         // bill only on the first day of the month
-        val isBillingPeriod = day == 1
-
+        val isBillingPeriod = day == 1;
         if (isBillingPeriod) {
 
             // get all the pending invoices
-            var invoicesToProcess = invoiceService.fetch()
-            var pending: List<Invoice> = invoicesToProcess
-                .filter { it.status == InvoiceStatus.PENDING.toString() }
+            var invoicesToProcess = invoiceService.fetchAllOfStatus(InvoiceStatus.PENDING)
 
-            // TODO try-catch and retry charge
             for (invoice in invoicesToProcess) {
 
-                // TODO pass relevant parameters to 'charge' (customer, currency, amount)
-                var charged = false
-                try {
+                var charged = paymentProvider.charge(invoice)
+                if (charged) {
 
-                    charged = PaymentProvider.charge(invoice)
-                    if (charged) {
-
-                        //TODO handle expetions/retry (customer should not be charged without
-                        // the invoice to be marked as 'paid'
-                        // mark invoice as 'PAID'
-                        invoiceService.update(invoice.id)
-                    }
-
-                } catch (e: CustomerNotFoundException) {
-                    // TODO call logger
-                } catch (e: CurrencyMismatchException) {
-                    // TODO call logger
-                } catch (e: NetworkException) {
-                    // TODO call logger
-                } catch (e: Exception) {
-                    // TODO call logger
+                    // mark the invoice as 'PAID'
+                    // as it is crucial that a charged invoice will not remain 'pending', attempt 3 times.
+                    updateBilledInvoice(invoice.id, retries = 3)
                 }
-
             }
         }
-
     }
-    
+
+    private fun updateBilledInvoice(invoiceId: Int, retries: Int){
+        try {
+            invoiceService.update(invoiceId, InvoiceStatus.PAID)
+
+        } catch (e: Exception){
+
+            var remaining = retries.minus(1)
+            if (remaining >= 0){
+                updateBilledInvoice(invoiceId, remaining)
+            }
+        }
+    }
+
 }
