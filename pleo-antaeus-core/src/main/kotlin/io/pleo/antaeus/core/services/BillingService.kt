f5cad6adc1ebf6d1a147a5580478932994e25e9a
JennyRockU <jennyrukman@gmail.com>
2019-06-08 14:18:39 +0300

    Added billing result return object, try-catch per invoice.

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,42 +1,80 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
+import org.junit.jupiter.api.extension.ExtensionContextException
 import java.time.LocalDate
 import java.time.ZoneOffset
 
 class BillingService(private val paymentProvider: PaymentProvider, private val invoiceService: InvoiceService) {
 
-    /*
-    Throws:
-    chargeInvoices can throw the following exceptions, which will be propagated to the caller (rest)
-          `CustomerNotFoundException`: when no customer has the given id.
-          `CurrencyMismatchException`: when the currency does not match the customer account.
-          `NetworkException`: when a network error happens.
-     */
-    fun chargeInvoices() {
-        val day = LocalDate.now(ZoneOffset.UTC).dayOfMonth
+    var billingDayOfMonth = 1
+    fun chargeInvoices() : BillingResult {
+
+        val today = LocalDate.now(ZoneOffset.UTC)
 
         // bill only on the first day of the month
-        val isBillingPeriod = day == 1;
-        if (isBillingPeriod) {
+        val isBillingPeriod = today.dayOfMonth == billingDayOfMonth
+        if (!isBillingPeriod){
+            val msg = "Invoices are only charged on the 1st of each month. Today is ${today.month} ${today.dayOfMonth}."
+            return BillingResult(message = msg)
+        }
 
-            // get all the pending invoices
-            var invoicesToProcess = invoiceService.fetchAllOfStatus(InvoiceStatus.PENDING)
+        // get all the pending invoices
+        val invoicesToProcess = invoiceService.fetchAllOfStatus(InvoiceStatus.PENDING)
 
-            for (invoice in invoicesToProcess) {
+        // init result data
+        val msg = "${invoicesToProcess.size} pending invoices were found"
+        var processed = 0
+        var failed = 0
+        var rejected = 0
 
-                var charged = paymentProvider.charge(invoice)
-                if (charged) {
+        // process all pending invoices
+        for (invoice in invoicesToProcess) {
+            // charge each invoice within a try-catch block to avoid failing
+            // the rest of the invoices
+            try {
+               val success = chargeInvoice(invoice)
 
-                    // mark the invoice as 'PAID'
-                    // as it is crucial that a charged invoice will not remain 'pending', attempt 3 times.
-                    updateBilledInvoice(invoice.id, retries = 3)
+                if (success){
+                    processed++
+
+                } else{
+                    rejected++
                 }
+
+            } catch (e: Exception){
+                //TODO log or send errors
+                failed++
             }
         }
+
+        return BillingResult(message = msg, processed = processed, failed = failed, rejected = rejected)
     }
 
+    internal fun chargeInvoice(invoice: Invoice): Boolean{
+        var charged = try {
+            paymentProvider.charge(invoice)
+
+        } catch (e: NetworkException){
+            //retry if failed for a network error
+            paymentProvider.charge(invoice)
+        }
+
+        if (charged) {
+
+            // mark the invoice as 'PAID'
+            // it's crucial that a charged invoice won't remain 'pending', so attempt 3 times.
+            updateBilledInvoice(invoice.id, retries = 3)
+        }
+
+        return charged
+    }
+
+    // attempts to charge the customer several times in case of a network exception
+    // other exceptions will be propagated to the caller
     private fun updateBilledInvoice(invoiceId: Int, retries: Int){
         try {
             invoiceService.update(invoiceId, InvoiceStatus.PAID)
@@ -44,10 +82,20 @@ class BillingService(private val paymentProvider: PaymentProvider, private val i
         } catch (e: Exception){
 
             var remaining = retries.minus(1)
-            if (remaining >= 0){
-                updateBilledInvoice(invoiceId, remaining)
+            if (remaining < 0){
+                // propagate the error if no retries left
+                throw e
             }
+            // if reached here, retry with reduced no. of tries.
+            updateBilledInvoice(invoiceId, remaining)
         }
     }
+
+    data class BillingResult(
+            val message: String,
+            val processed: Int = 0,
+            val rejected: Int = 0,
+            val failed: Int = 0
+    )
 
 }
